432050b748e63203c69cbbe0922924994686dd2f
Kenneth Palces <kennypal@Kenneths-MacBook-Pro.local>
2020-09-07 14:18:39 -0400

    finished creating components

--- my-grocery/src/components/Home.jsx
+++ my-grocery/src/components/Home.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import Archive from './Archive'
+import Create from './Create'
 
 const Home = () => {
   return (
     <div>
       <h1>Home</h1>
+      <Create />
+      <Archive />
     </div>
   );
 };
