8de4a66e7bf7f3687e53e97159895ad715715c88
Kenneth Palces <kennypal@Kenneths-MacBook-Pro.local>
2020-09-07 15:42:17 -0400

    stuck on getting data

--- my-grocery/src/components/Archive.jsx
+++ my-grocery/src/components/Archive.jsx
@@ -1,6 +1,23 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios'
 
 const Archive = () => {
+  const [groceries, setGroceries] = useState([]);
+
+  useEffect(() => {
+
+    const apiCall = async () => {
+      const airtableURL = `https://api.airtable.com/v0/${process.env.REACT_APP_AIRTABLE_BASE}/grocery?Grid%20View`
+      const response = await axios.get(airtableURL, {
+        headers: {
+          'Authorization': `Bearer ${process.env.REACT_APP_AIRTABLE_KEY}`,
+        },
+      });
+      console.log(response.data.records)
+      // setGroceries(response.data.records)
+    }
+    apiCall()
+  }, []);
   return (
     <div>
       <h1>Archive</h1>
