22e87d7a84a52d3cfa2fa44df28cf5363b36c63b
Kenneth Palces <kennypal@Kenneths-MacBook-Pro.local>
2020-09-08 10:54:43 -0400

    updated form

--- my-grocery/src/components/Create.jsx
+++ my-grocery/src/components/Create.jsx
@@ -1,18 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const Create = () => {
+const Create = (props) => {
+  const [list, setList] = useState('');
+  const [datePurchase, setDatePurchase] = useState('mm/dd/yyyy');
+  const [store, setStore] = useState('');
+  const [totalAmount, setTotalAmount] = useState('');
   return (
     <div>
       <h1>Create List</h1>
       <form>
         <label htmlFor="list">List:</label>
-        <textarea name="list" type="text"/>
+        <textarea
+          name="list"
+          type="text"
+          value={list}
+          onChange={(event) => setList(event.target.value)}
+        />
         <label htmlFor="datepurchase">Date Purchased:</label>
-        <input name="datepurchase" type="text"/>
+        <input
+          name="datepurchase"
+          type="text"
+          value={datePurchase}
+          onChange={(event) => setDatePurchase(event.target.value)}
+        />
         <label htmlFor="store">Store:</label>
-        <input name="store" type="text" />
+        <input
+          name="store"
+          type="text"
+          value={store}
+          onChange={(event) => setStore(event.target.value)}
+        />
         <label htmlFor="totalamount">Total Amount:</label>
-        <input name="totalmount" type="text" />
+        <input
+          name="totalamount"
+          type="text"
+          value={totalAmount}
+          onChange={(event) => setTotalAmount(event.target.value)}
+        />
         <button type="submit">Submit</button>
       </form>
     </div>
