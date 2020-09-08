1bc8979f79ab543935623d1b7aa2003b42752edc
Kenneth Palces <kennypal@Kenneths-MacBook-Pro.local>
2020-09-08 10:12:07 -0400

    form done

--- my-grocery/src/components/Create.jsx
+++ my-grocery/src/components/Create.jsx
@@ -5,12 +5,15 @@ const Create = () => {
     <div>
       <h1>Create List</h1>
       <form>
-        <label htmlFor=""></label>
-        <input type="text"/>
-        <label htmlFor=""></label>
-        <input type="text"/>
-        <label htmlFor=""></label>
-        <input type="text"/>
+        <label htmlFor="list">List:</label>
+        <textarea name="list" type="text"/>
+        <label htmlFor="datepurchase">Date Purchased:</label>
+        <input name="datepurchase" type="text"/>
+        <label htmlFor="store">Store:</label>
+        <input name="store" type="text" />
+        <label htmlFor="totalamount">Total Amount:</label>
+        <input name="totalmount" type="text" />
+        <button type="submit">Submit</button>
       </form>
     </div>
   );
