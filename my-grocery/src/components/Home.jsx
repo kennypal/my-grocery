7d8d4a7ff8f8f5012391a0bc2eee67278cb2f5da
Kenneth Palces <kennypal@Kenneths-MacBook-Pro.local>
2020-09-09 22:15:36 -0400

    added footer and some styling

--- my-grocery/src/components/Home.jsx
+++ my-grocery/src/components/Home.jsx
@@ -1,27 +1,32 @@
 import React from "react";
 import Archive from "./Archive";
-import Buttons from "./Buttons"
+import Buttons from "./Buttons";
 import Create from "./Create";
 import { Route, Link } from "react-router-dom";
 
 const Home = () => {
   return (
-    <div>
-      <Link to="/">
-        <div className="header">
-          <img src="https://i.imgur.com/0OBawMh.jpg" alt="header"></img>
-        </div>
-      </Link>
-      <Route exact path="/">
-        <Buttons />
-      </Route>
-      <Route path="/create">
+    <>
+      <div>
+        <Link to="/">
+          <div className="header">
+            <img src="https://i.imgur.com/0OBawMh.jpg" alt="header"></img>
+          </div>
+        </Link>
+        <Route exact path="/">
+          <Buttons />
+        </Route>
+        <Route path="/create">
           <Create />
-      </Route>
-      <Route path="/archive">
-        <Archive />
-      </Route>
-    </div>
+        </Route>
+        <Route path="/archive">
+          <Archive />
+        </Route>
+      </div>
+      <footer>
+        <div className="footer" />
+      </footer>
+    </>
   );
 };
 
