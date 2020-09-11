0c911b5d739a2cacc6e1996cf1e91dac75215b3f
Kenneth Palces <kennypal@Kenneths-MacBook-Pro.local>
2020-09-11 00:02:18 -0400

    finish with tablet and desktop

--- my-grocery/src/components/Archive.jsx
+++ my-grocery/src/components/Archive.jsx
@@ -27,14 +27,13 @@ const Archive = (props) => {
           <div className="background" key={grocery.id}>
             <div className="list">
               <div className="item-list">
-                <h3>{grocery.fields.datePurchase}</h3>
+                <h3>DATE PURCHASE: {grocery.fields.datePurchase}</h3>
+                <h3>ITEMS:</h3>
                   {grocery.fields.list.split("\n").map((item, idx) => (
                     <div className="item" key={idx}>{item}</div>
                   ))}
-                <h3>{grocery.fields.store}</h3>
-                <h3>
-                  Total Amount: <span>${grocery.fields.totalAmount}</span>
-                </h3>
+                <h3>STORE: {grocery.fields.store}</h3>
+                <h2>TOTAL AMOUNT: ${grocery.fields.totalAmount}</h2>
               </div>
             </div>
           </div>
